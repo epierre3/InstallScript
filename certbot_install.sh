2610ffa1969990feb8e1c76d1a9563d46ecda26d
epierre3 <36232374+epierre3@users.noreply.github.com>
2019-10-04 16:12:16 +0200

    Create certbot_install.sh

---
+++ certbot_install.sh
@@ -0,0 +1,4 @@
+    sudo apt-get update -y
+    sudo apt-get install software-properties-common -y
+    sudo add-apt-repository ppa:certbot/certbot
+    sudo apt-get update && sudo apt-get upgrade -y
