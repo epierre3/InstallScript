5130a8f7933b01fdf332381a75919200d1f78ee5
epierre3 <36232374+epierre3@users.noreply.github.com>
2020-01-23 21:44:03 +0100

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -3,7 +3,7 @@
 # Script for installing Odoo on Ubuntu 14.04, 15.04, 16.04 and 18.04 (could be used for other version too)
 # Author: Yenthe Van Ginneken
 #-------------------------------------------------------------------------------
-# This script will install Odoo on your Ubuntu 16.04 server. It can install multiple Odoo instances
+# This script will install Odoo on your Ubuntu 18.04 server. It can install multiple Odoo instances
 # in one Ubuntu because of the different xmlrpc_ports
 #-------------------------------------------------------------------------------
 # Make a new file:
